bff32cbe1bf7fe9af3b6ea0d1da84e97786c028b
syh8088 <syh8088@gmail.com>
2022-03-29 00:42:13 +0900

    쇼핑 상품 목록 페이지와 상세 페이지 개발
    쇼핑 상품 목록 페이지와 상세 페이지 개발

--- learn-nuxt/layouts/default.vue
+++ learn-nuxt/layouts/default.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
       <header>
-        <h1>{{ $route.name }}</h1>
+        <h1>
+          <NuxtLink to="/">Nuxt Shopping</NuxtLink>
+        </h1>
+        <!-- <h1>{{ $route.name }}</h1>
         <NuxtLink to="/">홈페이지</NuxtLink>
         <NuxtLink to="/main">메인 페이지</NuxtLink>
-        <NuxtLink to="/product">상품 페이지</NuxtLink>
+        <NuxtLink to="/product">상품 페이지</NuxtLink> -->
       </header>
       <Nuxt />
   </div>
--- learn-nuxt/pages/index.vue
+++ learn-nuxt/pages/index.vue
@@ -1,12 +1,103 @@
 <template>
-  <div>
-    <h1>홈 페이지</h1>
-    <p>넉스트 시작하기</p>
-  </div>
+    <!-- <ProductList></ProductList> -->
+    <div class="app">
+      <main>
+        <div>
+          <input type="text" />
+        </div>
+
+        <ul>
+          <li
+            v-for="product in products"
+            :key="product.id"
+            class="item flex"
+            @click="routeToDetailPage(product.id)"
+          >
+            <img class="product-image" :src="product.imageUrl" :alt="product.name">
+              <p>{{ product.name }}</p>
+              <span>{{ product.price }}</span>
+          </li>
+        </ul>
+      </main>
+    
+    </div>
 </template>
 
 <script>
+import axios from 'axios';
+// import ProductList from '~/components/ProductList.vue';
+
 export default {
-  
-}
+  // components: { ProductList },
+
+    async asyncData() {
+        const response = await axios.get('http://localhost:3000/products');
+        console.log("response", response);
+        const products = response.data.map((item) => {
+          return {
+            ...item,
+            imageUrl: `${item.imageUrl}?random=${Math.random()}`,
+          }
+        });
+
+        return { products };
+    },
+    data() {
+      return {
+        products: []
+      }
+    },
+
+    methods: {
+      // async filterItemsBySearchText() {
+      //   const { data } = await fetchProductsByKeyword(this.inputText)
+      //   this.items = data.map((item) => ({
+      //     ...item,
+      //     imageUrl: `${item.imageUrl}?random=${Math.random()}`,
+      //   }))
+      // },
+      routeToDetailPage(id) {
+        this.$router.push(`/detail/${id}`)
+      },
+      routeToCartPage() {
+        this.$router.push('/cart')
+      },
+    },
+// async created() {
+//   }
+ }
 </script>
+
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: center;
+}
+.item {
+  display: inline-block;
+  width: 400px;
+  height: 300px;
+  text-align: center;
+  margin: 0 0.5rem;
+  cursor: pointer;
+}
+.product-image {
+  width: 400px;
+  height: 250px;
+}
+.app {
+  position: relative;
+}
+.cart-wrapper {
+  position: sticky;
+  float: right;
+  bottom: 50px;
+  right: 50px;
+}
+.cart-wrapper .btn {
+  display: inline-block;
+  height: 40px;
+  font-size: 1rem;
+  font-weight: 500;
+}
+</style>
